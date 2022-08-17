c9067dac63ea0a6bd496ef23a0611216b1c47649
AnjishnuMandal <anjishnumandal2003@gmail.com>
2022-08-17 10:52:56 +0530

    Added Folders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import InstallPWA from './InstallPWA';
+import HomePage from './Pages/HomePage/HomePage';
 
 function App() {
   return (
     <>    
     <InstallPWA></InstallPWA>
+    <HomePage></HomePage>
     </>
   );
 }
---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <>
+    
+    <div className='italic text-6xl text-bold bg-red-600' >Anjishnu</div>
+    </>
+  )
+}
+
+export default HomePage
